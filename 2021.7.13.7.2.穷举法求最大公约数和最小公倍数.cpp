f582632d0db274b7621891b6dea63c596a79d778
yolo <77111763+yoloeww@users.noreply.github.com>
2021-07-13 18:58:10 +0800

    Add files via upload

---
+++ 2021.7.13.7.2.穷举法求最大公约数和最小公倍数.cpp
@@ -0,0 +1,31 @@
+# include<stdio.h>
+int max(int a,int b)
+{
+   int temp;
+   if(a>b)
+	   temp=a;
+   else
+	   temp=b;
+   for(int i=temp;i>0;i--)
+   {
+      if(a%i==0&&b%i==0)
+		  break;
+   }
+   return i;
+}
+int min(int a,int b)
+{
+	return a/b;
+}
+int main(void)
+{
+	int a,b,c,d;
+	printf("input a and b:\n");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	d=max(a,b);
+	printf("最大公约数为%d\n",d);
+    printf("最小公倍数为%d\n",c/d);
+	return 0;
+   
+}
