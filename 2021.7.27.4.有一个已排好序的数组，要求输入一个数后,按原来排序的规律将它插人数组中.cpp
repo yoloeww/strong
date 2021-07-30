cad24f93fb3083efb3c1ad48c63b548e181d7c49
yolo <77111763+yoloeww@users.noreply.github.com>
2021-07-30 22:11:25 +0800

    Add files via upload

--- 2021.7.27.4.有一个已排好序的数组，要求输入一个数后,按原来排序的规律将它插人数组中.cpp
+++ 2021.7.27.4.有一个已排好序的数组，要求输入一个数后,按原来排序的规律将它插人数组中.cpp
@@ -3,32 +3,24 @@
 int main(void)
 {
 
-	int a[N]={1,8,10};
+	int a[N]={1,8,10,38};
 	int n,t=0,i,j;
 	printf("请输入一个数:\n");
 	scanf("%d",&n);
-		if(n>a[N])
-			a[N]=n;
-		else
-		{
-	for(i=0;i<N-1;i++)
+	for(i=0;i<N;i++)
 	{
-	  if(a[i]>n)
-	  {
-	     t=a[i];
-		 a[i]=n;
-	  }
-	  for(j=i+1;j<N;j++)
-	  {
-	      int t2=a[j];
-		  a[j]=t;
-		  t=t2;
-	  }
-	  break;
+		if(a[i]>n)
+			break;
 	}
-		}
-		printf("the new array is \n");
-		for(i=0;i<N;i++)
-			printf("%d",a[i]);
+	for(j=N-1;j>=i;j--)
+	{
+			a[j+1]=a[j];
+	}
+		a[i]=n;
+
+	printf("the new array is \n");
+		for(i=0;i<N+1;i++)
+			printf("%5d",a[i]);
+		putchar('\n');
   return 0;	
 }
