9396c928c6d2ea885a646c4c5eeb732f96d22743
yolo <77111763+yoloeww@users.noreply.github.com>
2021-08-21 20:16:00 +0800

    Add files via upload

---
+++ 2021.8.21.17.用递归法将一个整数n转换成字符串。例如，输入483,应输出字符串483.cpp
@@ -0,0 +1,25 @@
+# include<stdio.h>
+void convert (int n)
+{
+   int i;
+   if((i=n/10)!=0)
+	   convert (i);
+   putchar(n%10+'0');
+ 
+}
+int main(void)
+{
+  int n;
+  printf("请输入一个整数n:\n");
+  scanf("%d",&n);
+  if(n<0)
+  {
+     putchar('-');
+	 putchar(' ');
+	 n=-n;
+  }
+  convert(n)
+;
+printf("\n");
+return 0;
+}
