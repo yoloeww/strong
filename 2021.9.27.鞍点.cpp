57082a7d568b05959a9a769b037e90ccba07add8
yolo <77111763+yoloeww@users.noreply.github.com>
2021-09-28 12:34:14 +0800

    Update 2021.9.27.鞍点.cpp

--- 2021.9.27.鞍点.cpp
+++ 2021.9.27.鞍点.cpp
@@ -1,1 +1,44 @@
-# include<stdio.h>int a
+# include<stdio.h>
+# define M 2
+# define N 2
+bool f(int a[][N],int i,int j)
+{
+       int x;
+	   for(x=0;x<M;x++)
+	   {
+		   if(a[x][j]<a[i][j])
+			   return false;
+	   }
+	   return true;
+}
+bool g(int a[][N],int i,int j)
+{
+       int x;
+	   for(x=0;x<N;x++)
+	   {
+		   if(a[i][x]>a[i][j])
+			   return false;
+	   }
+	   return true;
+}
+int main(void)
+{ 
+	int a[M][N],flag=0,i,j;
+	printf("array:\n");
+		for(i=0;i<M;i++)
+		for(j=0;j<N;j++)
+			scanf("%d",&a[i][j]);
+	for(i=0;i<M;i++)
+		for(j=0;j<N;j++)
+		{
+		    if(f(a,i,j)&&g(a,i,j))
+			{
+				flag=1;
+			printf("%d of 行:%d 列:%d",a[i][j],i,j);
+			}
+		}
+    printf("eror\n");
+	return 0;
+
+  
+}
