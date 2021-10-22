5d409e4dbb942a1dd0c557afb8768bb7b7ae29be
yolo <77111763+yoloeww@users.noreply.github.com>
2021-10-22 23:29:03 +0800

    Update 2021.10.19.杨辉.cpp

--- 2021.10.19.杨辉.cpp
+++ 2021.10.19.杨辉.cpp
@@ -1,1 +1,23 @@
-# include
+# include<stdio.h>
+# define N 10
+int main(void)
+{
+   int i,j,a[N][N];
+   for(i=0;i<N;i++)
+   {
+	   a[i][0]=1;
+	   a[i][i]=1;
+   }
+   for(i=2;i<N;i++)
+	   for(j=1;j<=i-1;j++)
+		   a[i][j]=a[i-1][j-1]+a[i-1][j];
+	     for(i=0;i<N;i++)
+		 {
+	   for(j=0;j<=i;j++)
+	   {
+		   printf("%5d",a[i][j]);
+	   }
+	   printf("\n");
+		 }
+	   return 0;
+}
