e549fbf535fb3cb5be7bda2c06189c59a3226edb
yolo <77111763+yoloeww@users.noreply.github.com>
2021-10-23 23:48:27 +0800

    Add files via upload

---
+++ 2021.10.22.复制.cpp
@@ -0,0 +1,20 @@
+# include<stdio.h>
+int main(void)
+{
+	char str1[100],str2[100];
+	char *p1,*p2;
+	printf("input two char:\n");
+	scanf("%s",str1);
+	scanf("%s",str2);
+	p1=str1;
+	p2=str2;
+	while(*p2!='\0')
+	{
+		*p1=*p2;
+		p1++;
+		p2++;
+	}
+	*p1='\0';
+    puts(str1);
+	return 0;
+}
