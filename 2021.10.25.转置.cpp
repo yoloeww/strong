81d280295aa517fa68e888bd42ac0c5404f720ba
yolo <77111763+yoloeww@users.noreply.github.com>
2021-10-25 23:51:40 +0800

    Update 2021.10.25.转置.cpp

--- 2021.10.25.转置.cpp
+++ 2021.10.25.转置.cpp
@@ -3,6 +3,24 @@
 int main(void)
 {
 	int a[M][M];
-
-   
+    int i,j,temp;
+	for(i=0;i<M;i++)
+		for(j=0;j<M;j++)
+			scanf("%d",&a[i][j]);
+   for(i=0;i<M;i++)
+	   for(j=i+1;j<M;j++)
+	   {
+	        temp=a[j][i];
+			a[j][i]=a[i][j];
+			a[i][j]=temp;
+	   }
+	   for(i=0;i<M;i++)
+	   {
+	   for(j=0;j<M;j++)
+	   {
+		   printf("%5d",a[i][j]);
+	   }
+	       putchar('\n');
+	   }
+	   return 0;
 }
