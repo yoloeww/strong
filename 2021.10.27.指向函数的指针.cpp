bc4ee99a334b1369410ac36507b15623465635ab
yolo <77111763+yoloeww@users.noreply.github.com>
2021-10-29 08:00:19 +0800

    Update 2021.10.27.指向函数的指针.cpp

--- 2021.10.27.指向函数的指针.cpp
+++ 2021.10.27.指向函数的指针.cpp
@@ -1,9 +1,22 @@
 # include<stdio.h>
 int main(void)
 {
-	int max(int ,int );
-	int max(int ,int );
-   int (*p)(int a,int b);
-    int a,b,c;
-
+  int max(int ,int);
+  int (*p)(int,int);
+  int a,b,c;
+  p=max;
+  printf("input a and b:\n");
+  scanf("%d%d",&a,&b);
+  c=(*p)(a,b);
+  printf("a=%d,b=%d\nmax=%d\n",a,b,c);
+  return 0;
+}
+int max(int x,int y)
+{
+   int z;
+   if(x>y)
+	   z=x;
+   else
+	   z=y;
+   return z;
 }
