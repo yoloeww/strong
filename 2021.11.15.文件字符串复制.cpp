8adf677cdcd92ed925a3918bf5209bab4337a089
yolo <77111763+yoloeww@users.noreply.github.com>
2021-11-16 15:42:50 +0800

    Update 2021.11.15.文件字符串复制.cpp

--- 2021.11.15.文件字符串复制.cpp
+++ 2021.11.15.文件字符串复制.cpp
@@ -1,1 +1,26 @@
 # include<stdio.h>
+# include<stdlib.h>
+# include<malloc.h>
+# include<string.h>
+int main(void)
+{
+   FILE *fp;
+   fp=fopen("D:\\古诗.txt","w");
+   if(fp==NULL)
+   {
+     printf("error\n");
+	 exit(-1);
+   }
+   char stu[3][80],ch[80];
+   int i,j,max;
+   printf("input char:\n");
+   for(i=0;i<3;i++)
+	   gets(stu[i]);
+   printf("new char:\n");
+   for(i=0;i<3;i++)
+   {
+     fputs(stu[i],fp);
+	 puts(stu[i]);
+   }
+   return 0;
+} 
