9026f75855a40c44d0e9dd5f9e60f9384310393a
yolo <77111763+yoloeww@users.noreply.github.com>
2021-11-22 21:48:09 +0800

    Update 2021.11.21.乒乓球.cpp

--- 2021.11.21.乒乓球.cpp
+++ 2021.11.21.乒乓球.cpp
@@ -1,1 +1,13 @@
-# include
+# include<stdio.h>
+int main(void)
+{
+  char a,b,c;
+  for(a='x';a<='z';a++)
+	  for(b='x';b<='z';b++)
+          	  if(a!=b)
+				  for(c='x';c<='z';c++)
+					  if(c!=a&&c!=b)
+						  if((a!='x')&&(c!='x')&&(c!='z'))
+							 	printf("A=>%c,B->%c,C->%c\n",a,b,c);
+						  return 0;
+} 
